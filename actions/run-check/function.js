53f4d3b3800c5bcb7af95e9adf293d0a18bb71b4
Andrew Catton <>
2018-06-05 02:19:38 +0000

    Changed this skill to work room by room, with an "is all good" option for efficiency

--- actions/run-check/function.js
+++ actions/run-check/function.js
@@ -2,7 +2,7 @@ function(contextString, ellipsis) {
   const Context = require('context');
 const context = new Context(JSON.parse(contextString));
 
-ellipsis.success(`Sensory check for \`${context.currentCrop}\` in \`${context.currentRoom}\`:`, {
+ellipsis.success(`Sensory check for \`${context.nextCrop()}\` in \`${context.room}\`:`, {
   next: {
     actionName: "run-result-check",
     args: [
